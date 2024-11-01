8f7da72899e9bc45f4a93f609d9dae5e11da7e99
Stephan Lüddemann <s.lueddemann@mailbox.org>
2024-11-01 11:29:58 +0100

    :recycle: refactor: Welcome animation

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -14,27 +14,48 @@ function Welcome({ onLetUsRole }: Props) {
 
   return (
     <div className="h-full w-full bg-white flex  justify-center items-center rounded-md">
-      <div className="flex flex-col items-center max-w-xl text-center">
+      <div className="flex gap-4 flex-col items-center max-w-xl text-center">
         <div className="flex gap-4">
           <motion.div
-            className="origin-bottom-right"
-            initial={{ rotate: 0 }}
-            animate={{ rotate: 15 }}
-            transition={{
-              duration: 0.5,
-              repeat: Infinity,
-              repeatType: "reverse",
-            }}>
-            <TypographyH1>👋</TypographyH1>
+            initial={{ opacity: 0, x: -8 }}
+            animate={{ opacity: 1, x: 0 }}>
+            <motion.div
+              className="origin-bottom-right"
+              initial={{ rotate: 0 }}
+              animate={{ rotate: 15 }}
+              transition={{
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "reverse",
+              }}>
+              <TypographyH1>👋</TypographyH1>
+            </motion.div>
           </motion.div>
-          <TypographyH1>{t("title")}</TypographyH1>
+
+          <div className="flex gap-0.5">
+            {t("title")
+              .split("")
+              .map((letter, index) => (
+                <motion.h1
+                  initial={{ opacity: 0, x: -8 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ delay: index * 0.02 }}
+                  className="scroll-m-20 min-w-2 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                  {letter}
+                </motion.h1>
+              ))}
+          </div>
         </div>
 
-        <TypographyP>{t("description")}</TypographyP>
-
-        <Button onClick={() => onLetUsRole()} size="lg" className="mt-4">
-          {t("letsRole")} <FaDiceD20 />
-        </Button>
+        <motion.div
+          initial={{ opacity: 0, y: "2rem" }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}>
+          <TypographyP>{t("description")}</TypographyP>
+          <Button onClick={() => onLetUsRole()} size="lg" className="mt-4">
+            {t("letsRole")} <FaDiceD20 />
+          </Button>
+        </motion.div>
       </div>
     </div>
   );
