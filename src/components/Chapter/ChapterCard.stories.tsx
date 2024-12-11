283d7a07dd6ea36cea42f2d8e3bd7c99df9b1448
lw-lueddemann <sl@lichtwart.io>
2024-12-11 20:48:11 +0100

    :sparkle: feat: ChapterCard

---
+++ src/components/Chapter/ChapterCard.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import ChapterCard from "./ChapterCard";
+
+const meta = {
+  title: "Components/ChapterCard",
+  component: ChapterCard,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    onEdit: fn(),
+    chapter: {
+      battlemap: null,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sequi, minus iure quidem temporibus saepe fugit dolore? Quaerat repudiandae culpa eius, sint voluptatum laudantium voluptas quod ratione sapiente, laboriosam hic! Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sequi, minus iure quidem temporibus saepe fugit dolore? Quaerat repudiandae culpa eius, sint voluptatum laudantium voluptas quod ratione sapiente, laboriosam hic!",
+      encounters: null,
+      experience: 200,
+      icon: "⚔️",
+      id: 1,
+      name: "1. a name of the chapter",
+      state: "draft",
+      tokens: null,
+    },
+  },
+  decorators: [(Story) => <div className="bg-white p-4">{Story()}</div>],
+} satisfies Meta<typeof ChapterCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  // args: {},
+};
---
+++ src/components/Chapter/ChapterCard.tsx
@@ -0,0 +1,51 @@
+import { Chapter } from "@/types/chapters";
+import { motion } from "framer-motion";
+import { TypographyH2 } from "../ui/typographyh2";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+import { TypographyP } from "../ui/typographyP";
+import { RiEdit2Fill } from "react-icons/ri";
+
+type Props = {
+  chapter: Chapter;
+  onEdit: (chapterId: Chapter["id"]) => void;
+};
+
+function ChapterCard({ chapter, onEdit }: Props) {
+  function handleEdit() {
+    onEdit(chapter.id);
+  }
+
+  return (
+    <motion.div
+      className="flex w-full flex-col gap-4"
+      initial={{ opacity: 0, x: "-2rem" }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: "2rem" }}
+    >
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between gap-2">
+          <TypographyH2>{chapter.name}</TypographyH2>
+
+          <Button
+            aria-label={`Edit chapter ${chapter.name}`}
+            onClick={handleEdit}
+            variant="ghost"
+            size="icon"
+          >
+            <RiEdit2Fill />
+          </Button>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Badge>{chapter.state}</Badge>
+          {chapter.experience && <Badge>{chapter.experience} EP</Badge>}
+        </div>
+      </div>
+
+      <TypographyP className="line-clamp-6">{chapter.description}</TypographyP>
+    </motion.div>
+  );
+}
+
+export default ChapterCard;
