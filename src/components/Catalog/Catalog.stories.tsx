dc66a7fc1c1b45f2acebf0fc9989715eda8f0d1b
lw-lueddemann <sl@lichtwart.io>
2024-12-26 15:16:00 +0100

    :sparkle: feat: Catalog

---
+++ src/components/Catalog/Catalog.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import Catalog from "./Catalog";
+import ImmunityCard from "../ImmunityCard/ImmunityCard";
+
+const immunities = [
+  {
+    id: 1,
+    description: `
+- 4 damage over time decreased
+- ignores water damage`,
+    name: "Feuerimunität",
+    icon: "🔥",
+  },
+  {
+    id: 2,
+    description: `
+- no **splash damage**,
+- ignores water damage`,
+    name: "Water Splash ",
+    icon: "💧",
+  },
+  {
+    id: 3,
+    description: `
+- no **splash damage**,
+- ignores water damage`,
+    name: "Water Splash ",
+    icon: "💧",
+  },
+  {
+    id: 4,
+    description: `
+- no **splash damage**,
+- ignores water damage`,
+    name: "Water Splash ",
+    icon: "💧",
+  },
+  {
+    id: 5,
+    description: `
+- no **splash damage**,
+- ignores water damage`,
+    name: "Water Splash ",
+    icon: "💧",
+  },
+];
+
+const meta = {
+  title: "Components/Catalog",
+  component: Catalog,
+  parameters: {},
+  args: {
+    triggerName: "Immunities Catalog",
+    disabled: false,
+    description: "Choose an immunity for your player.",
+    title: "Immunity Catalog",
+    onSearchChange: fn(),
+    children: (
+      <div className="flex flex-col gap-2">
+        {immunities.map((immunity) => (
+          <ImmunityCard key={immunity.id} immunity={immunity} onAdd={fn()} />
+        ))}
+      </div>
+    ),
+  },
+  decorators: [
+    (Story: StoryFn) => (
+      <div className="h-fit w-full rounded-md bg-white p-2">{Story()}</div>
+    ),
+  ],
+} satisfies Meta<typeof Catalog>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  // args: {},
+};
---
+++ src/components/Catalog/Catalog.tsx
@@ -0,0 +1,61 @@
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+
+type Props = {
+  /**
+   * Name for the button which opens the catalog dialog
+   */
+  triggerName: string;
+  title: string;
+  description: string;
+  /** Disables the catalog open trigger */
+  disabled?: boolean;
+  children: React.ReactNode;
+  onSearchChange: (search: string) => void;
+};
+
+function Catalog({
+  triggerName,
+  title,
+  description,
+  disabled,
+  children,
+  onSearchChange,
+}: Props) {
+  function handleSearchTerm(event: React.ChangeEvent<HTMLInputElement>) {
+    onSearchChange(event.target.value);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button disabled={disabled}>{triggerName}</Button>
+      </DialogTrigger>
+
+      <DialogContent className="p-4 pr-0">
+        <div className="pr-4">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+
+            <DialogDescription>{description}</DialogDescription>
+          </DialogHeader>
+
+          <Input placeholder="search" onChange={handleSearchTerm} />
+        </div>
+
+        <div className="max-h-[500px] overflow-hidden">{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default Catalog;
