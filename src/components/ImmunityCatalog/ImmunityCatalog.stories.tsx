1575041f313a8533e71da48fd63197e13ecb8c5f
lw-lueddemann <sl@lichtwart.io>
2024-12-27 22:30:45 +0100

    :sparkle: feat: ImmunityCatalog

---
+++ src/components/ImmunityCatalog/ImmunityCatalog.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import Collapsible from "./ImmunityCatalog";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DBImmunity } from "@/types/immunitiy";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: Infinity, refetchOnMount: true } },
+});
+
+const meta = {
+  title: "Components/ImmunityCatalog",
+  component: Collapsible,
+  parameters: {},
+  args: {
+    onAddImmunity: fn(),
+    //@ts-expect-error
+    query: {
+      data: [
+        { description: "description", icon: "🔥", id: 1, name: "fire" },
+        { description: "description", icon: "💧", id: 2, name: "water" },
+      ],
+    },
+    selection: [],
+  },
+  decorators: [
+    (Story) => (
+      <div className="h-fit w-full rounded-md bg-white p-2">{Story()}</div>
+    ),
+    (Story) => {
+      queryClient.setQueryData(["immunities"], {
+        description: "some immunity description",
+        icon: "🔥",
+        id: 1,
+        name: "Fire Immunity",
+      } as DBImmunity);
+      return (
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      );
+    },
+  ],
+} satisfies Meta<typeof Collapsible>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  // args: {
+  // },
+};
+
+export const Loading: Story = {
+  args: {
+    // @ts-expect-error
+    query: { isLoading: true },
+  },
+};
+
+export const WithSelection: Story = {
+  args: {
+    selection: [
+      { description: "description", icon: "🔥", id: 1, name: "fire" },
+    ],
+  },
+};
---
+++ src/components/ImmunityCatalog/ImmunityCatalog.tsx
@@ -0,0 +1,79 @@
+import { DBImmunity } from "@/types/immunitiy";
+import { UseQueryResult } from "@tanstack/react-query";
+import ImmunityCard from "../ImmunityCard/ImmunityCard";
+import { Button } from "../ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import Catalog from "../Catalog/Catalog";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { useState } from "react";
+import Loader from "../Loader/Loader";
+
+type Props = {
+  query: UseQueryResult<DBImmunity[], unknown>;
+  selection: DBImmunity[];
+  onAddImmunity: (immunity: DBImmunity) => void;
+};
+
+function ImmunityCatalog({ query, selection, onAddImmunity }: Props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearchTerm(search: string) {
+    setSearchTerm(search);
+  }
+
+  return (
+    <>
+      {!query.isLoading && query?.data && (
+        <Catalog
+          disabled={false}
+          triggerName={"Catalog"}
+          title={"Immunities"}
+          description={"Select an immunity for the character to add. "}
+          onSearchChange={handleSearchTerm}
+          children={
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-4 p-0.5 pr-4">
+                {query.data
+                  .filter((immunity) =>
+                    immunity.name
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase()),
+                  )
+                  .filter(
+                    (immunity) =>
+                      !selection.some((item) => item.id === immunity.id),
+                  )
+                  .map((immunity) => (
+                    <ImmunityCard
+                      key={immunity.id}
+                      immunity={immunity}
+                      actions={
+                        <Button
+                          size="icon"
+                          onClick={() => onAddImmunity(immunity)}
+                        >
+                          <PlusIcon />
+                        </Button>
+                      }
+                    />
+                  ))}
+              </div>
+            </ScrollArea>
+          }
+        />
+      )}
+
+      {!query.isLoading && !query?.data && "nothing found create one"}
+
+      {query.isLoading && (
+        <Loader
+          size="large"
+          title="Loading Immunities..."
+          key="loader-immunity-catalog"
+        />
+      )}
+    </>
+  );
+}
+
+export default ImmunityCatalog;
