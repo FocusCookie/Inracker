3bad94fb7d282364093cbd708af65bd0189cff31
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 11:07:06 +0100

    :recycle: refactor: PartyCard

--- src/components/PlayerCard/PlayerCard.tsx
+++ src/components/PlayerCard/PlayerCard.tsx
@@ -12,25 +12,57 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { DBImmunity } from "@/types/immunitiy";
+import { DBResistance } from "@/types/resistances";
+import ImmunityCard from "../ImmunityCard/ImmunityCard";
+import { Button } from "../ui/button";
+import ResistanceCard from "../ResistanceCard/ResistanceCard";
+import IconAvatar from "../IconAvatar/IconAvatar";
 
 type Props = {
   player: Player;
   expanded: boolean;
   onRemove: (playerId: Player["id"]) => void;
-  onEdit: (player: Player["id"]) => void;
+  onEdit: (player: Player) => void;
+  onAddImmunity: (player: Player) => void;
+  onAddResistance: (player: Player) => void;
+  onRemoveImmunity: (
+    playerId: Player["id"],
+    immunityId: DBImmunity["id"],
+  ) => void;
+  onRemoveResistance: (
+    playerId: Player["id"],
+    resistanceId: DBResistance["id"],
+  ) => void;
 };
 
-function PlayerCard({ player, expanded, onEdit, onRemove }: Props) {
-  const handleRemovePlayer = () => {
+function PlayerCard({
+  player,
+  expanded,
+  onEdit,
+  onRemove,
+  onAddImmunity,
+  onAddResistance,
+  onRemoveImmunity,
+  onRemoveResistance,
+}: Props) {
+  function handleRemovePlayer() {
     onRemove(player.id);
-  };
+  }
 
-  const handleEditPlayer = () => {
-    onEdit(player.id);
-  };
+  function handleEditPlayer() {
+    onEdit(player);
+  }
+
+  function handleRemoveImmunity(immunityId: DBImmunity["id"]) {
+    onRemoveImmunity(player.id, immunityId);
+  }
+
+  function handleRemoveResistance(resistanceId: DBResistance["id"]) {
+    onRemoveResistance(player.id, resistanceId);
+  }
 
   //TODO: Translations
   const quickActions = () => (
@@ -38,21 +70,12 @@ function PlayerCard({ player, expanded, onEdit, onRemove }: Props) {
       <DropdownMenuLabel>{player.name}</DropdownMenuLabel>
       <DropdownMenuSeparator />
       <DropdownMenuGroup>
-        <DropdownMenuItem>
-          Set Initiative
-          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+        <DropdownMenuItem>Add Effect</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => onAddImmunity(player)}>
+          Add Immunity
         </DropdownMenuItem>
-        <DropdownMenuItem>
-          Decrease Health
-          <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          Increase Health
-          <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          Add Effect
-          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+        <DropdownMenuItem onClick={() => onAddResistance(player)}>
+          Add Resistance
         </DropdownMenuItem>
       </DropdownMenuGroup>
 
@@ -66,12 +89,120 @@ function PlayerCard({ player, expanded, onEdit, onRemove }: Props) {
     </DropdownMenuContent>
   );
 
-  return expanded ? (
-    <div className="flex h-full w-[576px] gap-2">
-      <div className="w-16">
+  return (
+    <>
+      {expanded ? (
+        <div className="flex h-full w-[576px] gap-2">
+          <div className="w-16">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="focus-visible:ring-ring hover:bg-accent relative grid h-16 w-16 flex-col place-content-center gap-1 rounded-md ring-offset-1 hover:cursor-pointer focus-visible:ring-1 focus-visible:outline-hidden">
+                  <IconAvatar player={player} />
+                </button>
+              </DropdownMenuTrigger>
+              {quickActions()}
+            </DropdownMenu>
+          </div>
+
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.3, delay: 0.15 },
+            }}
+            className="flex w-full flex-col gap-2 pt-0.5"
+          >
+            <div className="grow">
+              <div className="flex flex-col py-1">
+                <div className="flex items-center gap-2">
+                  <span className="grow text-xl font-bold">{player.name}</span>
+
+                  <Badge>LVL {player.level}</Badge>
+                  <Badge>
+                    <div className="flex items-center gap-1">
+                      <HeartFilledIcon />
+                      <span>
+                        {player.health}/{player.max_health}
+                      </span>
+                    </div>
+                  </Badge>
+                </div>
+                <span className="">{player.role}</span>
+              </div>
+            </div>
+
+            <Collapsible title="Overview">
+              <MarkdownReader markdown={player.overview} />
+            </Collapsible>
+
+            <Collapsible title="Details">
+              <MarkdownReader markdown={player.details} />
+            </Collapsible>
+
+            {player.immunities.length > 0 && (
+              <Collapsible
+                title={
+                  <div className="items-cent flex justify-between gap-2">
+                    <span>Immunities</span>
+                    <span className="rounded-md bg-black px-2 py-1 text-sm text-white">
+                      {player.immunities.length}
+                    </span>
+                  </div>
+                }
+              >
+                <div className="flex w-full flex-col gap-4">
+                  {player.immunities.map((immunity) => (
+                    <ImmunityCard
+                      key={`player-${player.id}-immunity-${immunity.id}`}
+                      immunity={immunity}
+                      actions={
+                        <Button
+                          onClick={() => handleRemoveImmunity(immunity.id)}
+                        >
+                          Remove
+                        </Button>
+                      }
+                    />
+                  ))}
+                </div>
+              </Collapsible>
+            )}
+
+            {player.resistances.length > 0 && (
+              <Collapsible
+                title={
+                  <div className="items-cent flex justify-between gap-2">
+                    <span>Resistances</span>
+                    <span className="rounded-md bg-black px-2 py-1 text-sm text-white">
+                      {player.resistances.length}
+                    </span>
+                  </div>
+                }
+              >
+                <div className="flex w-full flex-col gap-4">
+                  {player.resistances.map((resistance) => (
+                    <ResistanceCard
+                      key={`player-${player.id}-resistances-${resistance.id}`}
+                      resistance={resistance}
+                      actions={
+                        <Button
+                          onClick={() => handleRemoveResistance(resistance.id)}
+                        >
+                          Remove
+                        </Button>
+                      }
+                    />
+                  ))}
+                </div>
+              </Collapsible>
+            )}
+          </motion.div>
+        </div>
+      ) : (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <button className="focus-visible:ring-ring hover:bg-accent relative grid h-16 w-16 flex-col place-content-center gap-1 rounded-md ring-offset-1 hover:cursor-pointer focus-visible:ring-1 focus-visible:outline-hidden">
+            <button className="focus-visible:ring-ring hover:bg-accent relative flex w-16 flex-col items-center gap-1 rounded-md ring-offset-1 hover:cursor-pointer focus-visible:ring-1 focus-visible:outline-hidden">
               <div className="hover:bg-accent relative grid h-16 w-16 place-content-center rounded-md">
                 <Avatar>
                   <AvatarImage
@@ -85,93 +216,42 @@ function PlayerCard({ player, expanded, onEdit, onRemove }: Props) {
                   {player.icon}
                 </span>
               </div>
+
+              <div className="flex items-center gap-[1px] rounded-md bg-gray-200 px-1 py-0.5 text-sm font-bold text-black">
+                <span>{player.health}</span>
+                <span>/</span>
+                <span>{player.max_health}</span>
+              </div>
+
+              <div className="flex w-full justify-between gap-2">
+                {player.effects.filter((effect) => effect.type === "positive")
+                  .length > 0 && (
+                  <div className="w-full rounded-md bg-emerald-500 px-1 py-0.5 text-sm font-bold text-white">
+                    {
+                      player.effects.filter(
+                        (effect) => effect.type === "positive",
+                      ).length
+                    }
+                  </div>
+                )}
+                {player.effects.filter((effect) => effect.type === "negative")
+                  .length > 0 && (
+                  <div className="w-full rounded-md bg-red-500 px-1 py-0.5 text-sm font-bold text-white">
+                    {
+                      player.effects.filter(
+                        (effect) => effect.type === "negative",
+                      ).length
+                    }
+                  </div>
+                )}
+              </div>
             </button>
           </DropdownMenuTrigger>
+
           {quickActions()}
         </DropdownMenu>
-      </div>
-
-      <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        animate={{
-          opacity: 1,
-          x: 0,
-          transition: { duration: 0.3, delay: 0.15 },
-        }}
-        className="flex w-full flex-col gap-2 pt-0.5"
-      >
-        <div className="grow">
-          <div className="flex flex-col py-1">
-            <div className="flex items-center gap-2">
-              <span className="grow text-xl font-bold">{player.name}</span>
-
-              <Badge>LVL {player.level}</Badge>
-              <Badge>
-                <div className="flex items-center gap-1">
-                  <HeartFilledIcon />
-                  <span>
-                    {player.health}/{player.max_health}
-                  </span>
-                </div>
-              </Badge>
-            </div>
-            <span className="">{player.role}</span>
-          </div>
-        </div>
-
-        <div className="overflow-hidden p-0.5">
-          <Collapsible title="Overview">
-            <MarkdownReader markdown={player.overview} />
-          </Collapsible>
-        </div>
-      </motion.div>
-    </div>
-  ) : (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className="focus-visible:ring-ring hover:bg-accent relative flex w-16 flex-col gap-1 rounded-md ring-offset-1 hover:cursor-pointer focus-visible:ring-1 focus-visible:outline-hidden">
-          <div className="hover:bg-accent relative grid h-16 w-16 place-content-center rounded-md">
-            <Avatar>
-              <AvatarImage src={player.image || undefined} alt={player.name} />
-              <AvatarFallback>{player.icon}</AvatarFallback>
-            </Avatar>
-
-            <span className="absolute top-0 right-0 rounded-full bg-white p-0.5 shadow">
-              {player.icon}
-            </span>
-          </div>
-
-          <div className="flex items-center gap-[1px] rounded-md bg-gray-200 px-1 py-0.5 text-sm font-bold text-black">
-            <span>{player.health}1</span>
-            <span>/</span>
-            <span>{player.max_health}4</span>
-          </div>
-
-          <div className="flex w-full justify-between gap-2">
-            {player.effects.filter((effect) => effect.type === "positive")
-              .length > 0 && (
-              <div className="w-full rounded-md bg-emerald-500 px-1 py-0.5 text-sm font-bold text-white">
-                {
-                  player.effects.filter((effect) => effect.type === "positive")
-                    .length
-                }
-              </div>
-            )}
-            {player.effects.filter((effect) => effect.type === "negative")
-              .length > 0 && (
-              <div className="w-full rounded-md bg-red-500 px-1 py-0.5 text-sm font-bold text-white">
-                {
-                  player.effects.filter((effect) => effect.type === "negative")
-                    .length
-                }
-              </div>
-            )}
-          </div>
-        </button>
-      </DropdownMenuTrigger>
-
-      {quickActions()}
-    </DropdownMenu>
+      )}
+    </>
   );
 }
 
