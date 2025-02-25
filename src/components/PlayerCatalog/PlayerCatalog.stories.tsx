4d85426bac7e87396d7a0fff4003e45533c13ba8
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 11:20:02 +0100

    :sparkle: feat: PlayerCatalog

--- src/components/PlayerCatalog/PlayerCatalog.stories.tsx
+++ src/components/PlayerCatalog/PlayerCatalog.stories.tsx
@@ -1,12 +1,35 @@
 import type { Meta, StoryObj } from "@storybook/react";
-// import { fn } from "@storybook/test";
-
+import { fn } from "@storybook/test";
 import PlayerCatalog from "./PlayerCatalog";
 
 const meta = {
   title: "Components/PlayerCatalog",
   component: PlayerCatalog,
-  args: {},
+  args: {
+    open: true,
+    onOpenChange: fn(),
+    onAdd: fn(),
+    partyId: 99,
+    players: [
+      {
+        details: "details",
+        effects: [],
+        ep: 123,
+        health: 10,
+        icon: "💰",
+        id: 1,
+        image: null,
+        immunities: [],
+        level: 2,
+        max_health: 22,
+        name: "tester",
+        overview: "overview",
+        resistances: [],
+        role: "tester",
+      },
+    ],
+    excludedPlayers: [],
+  },
 } satisfies Meta<typeof PlayerCatalog>;
 
 export default meta;
--- src/components/PlayerCatalog/PlayerCatalog.tsx
+++ src/components/PlayerCatalog/PlayerCatalog.tsx
@@ -1,7 +1,8 @@
+import { Party } from "@/types/party";
+import { Player } from "@/types/player";
 import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Badge } from "../ui/badge";
-import { Player } from "@/types/player";
 import {
   Dialog,
   DialogContent,
@@ -11,45 +12,45 @@ import {
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { ScrollArea } from "../ui/scroll-area";
+import { useTranslation } from "react-i18next";
 
 type Props = {
+  partyId: Party["id"];
   open: boolean;
   onOpenChange: (state: boolean) => void;
-  onAdd: (player: Player["id"]) => void;
+  onAdd: (partyId: Party["id"], player: Player["id"]) => void;
   excludedPlayers: Player[];
   players: Player[];
 };
 
 function PlayerCatalog({
+  partyId,
   open,
-  onOpenChange,
   excludedPlayers,
-  onAdd,
   players,
+  onAdd,
+  onOpenChange,
 }: Props) {
+  const { t } = useTranslation("ComponentPlayerCatalog");
   const [search, setSearch] = useState("");
 
   function handleSearchTerm(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
   }
 
-  //TODO: Add translations
   return (
     <Dialog onOpenChange={onOpenChange} open={open}>
       <DialogContent className="m-4 p-4 pr-0">
         <div className="pr-4">
           <DialogHeader>
-            <DialogTitle>Add a player to your group</DialogTitle>
+            <DialogTitle>{t("title")}</DialogTitle>
 
-            <DialogDescription>
-              Recruit a hero from our catalog of adventure companions to join
-              your team!
-            </DialogDescription>
+            <DialogDescription>{t("description")}</DialogDescription>
           </DialogHeader>
 
           <Input
             className="mt-4"
-            placeholder="Search for a specific hero..."
+            placeholder={t("search")}
             onChange={handleSearchTerm}
           />
         </div>
@@ -70,21 +71,25 @@ function PlayerCatalog({
                 .map((player) => (
                   <button
                     key={player.id}
-                    onClick={() => onAdd(player.id)}
+                    onClick={() => onAdd(partyId, player.id)}
                     className="focus-visible:ring-ring hover:bg-secondary/80 focus-within:bg-secondary/80 flex w-full items-center justify-start gap-2 rounded-md p-1 ring-offset-1 outline-black transition-colors focus-within:outline-1 hover:cursor-pointer focus-visible:ring-1"
                   >
-                    <Avatar>
-                      <AvatarImage
-                        src={`src(${player.image})`}
-                        alt={player.name}
-                      />
-                      <AvatarFallback>{player.name.slice(0, 2)}</AvatarFallback>
-                    </Avatar>
+                    <div className="hover:bg-accent relative grid h-16 w-16 place-content-center rounded-md">
+                      <Avatar>
+                        <AvatarImage
+                          src={player.image || undefined}
+                          alt={player.name}
+                        />
+                        <AvatarFallback>{player.icon}</AvatarFallback>
+                      </Avatar>
+
+                      <span className="absolute top-0 right-0 rounded-full bg-white p-0.5 shadow">
+                        {player.icon}
+                      </span>
+                    </div>
 
                     <div className="flex grow flex-col items-start justify-start">
-                      <span className="text-xl font-bold">
-                        {player.icon} {player.name}
-                      </span>
+                      <span className="text-xl font-bold">{player.name}</span>
                     </div>
 
                     <Badge variant="outline">{player.role}</Badge>
