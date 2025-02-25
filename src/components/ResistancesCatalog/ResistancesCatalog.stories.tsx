54983ee78e6ac32adc0519515708814a90d15675
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 11:26:38 +0100

    :recycle: refactor: ResistanceCatalog Story

--- src/components/ResistancesCatalog/ResistancesCatalog.stories.tsx
+++ src/components/ResistancesCatalog/ResistancesCatalog.stories.tsx
@@ -1,12 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
-// import { fn } from "@storybook/test";
+import { fn } from "@storybook/test";
 
 import ResistancesCatalog from "./ResistancesCatalog";
 
 const meta = {
   title: "Components/ResistancesCatalog",
   component: ResistancesCatalog,
-  args: {},
+  args: {
+    onAdd: fn(),
+    onOpenChange: fn(),
+    open: true,
+    resistances: [
+      { description: "description text", icon: "📞", id: 1, name: "telefon" },
+      { description: "description text", icon: "💰", id: 2, name: "money" },
+    ],
+  },
 } satisfies Meta<typeof ResistancesCatalog>;
 
 export default meta;
