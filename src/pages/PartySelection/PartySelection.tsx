270fc5ff2d963d0dee2e882e35adc16f8489afe6
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 12:08:38 +0100

    :recycle: refactor: PartySelection

--- src/pages/PartySelection/PartySelection.tsx
+++ src/pages/PartySelection/PartySelection.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import Loader from "@/components/Loader/Loader";
 import PartyCard from "@/components/PartyCard/PartyCard";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
 import { TypographyH1 } from "@/components/ui/typographyH1";
 import { TypographyP } from "@/components/ui/typographyP";
-import { usePartiesStore } from "@/stores/PartiesStores";
 import { Party } from "@/types/party";
-import { useNavigate } from "@tanstack/react-router";
 import { AnimatePresence } from "framer-motion";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 type PartySelectionProps = {
@@ -16,23 +14,17 @@ type PartySelectionProps = {
   children?: React.ReactNode;
   onEditParty: (party: Party) => void;
   onPartySelect: (id: Party["id"]) => void;
+  onCreateParty: () => void;
 };
 
 const PartySelection = ({
   parties,
   loading,
-  children,
   onEditParty,
   onPartySelect,
+  onCreateParty,
 }: PartySelectionProps) => {
   const { t } = useTranslation("PagePartySelection");
-
-  const drawerChild = React.Children.toArray(children).find((child) => {
-    return (
-      React.isValidElement(child) &&
-      (child.type as any).displayName === "PartySelectionCreateDrawer"
-    );
-  });
 
   function handlePartySelect(partyId: Party["id"]) {
     onPartySelect(partyId);
@@ -42,16 +34,23 @@ const PartySelection = ({
     <div className="flex h-full w-full flex-col items-center gap-8 rounded-md bg-white p-2">
       <div className="flex max-w-xl flex-col gap-2">
         <TypographyH1>{t("headline")}</TypographyH1>
+
         <TypographyP>{t("description")}</TypographyP>
-        <div className="flex w-full justify-center">{drawerChild}</div>
       </div>
+
+      <Button
+        onClick={onCreateParty}
+        variant={parties.length === 0 ? "default" : "outline"}
+      >
+        {t("createParty")}
+      </Button>
 
       <AnimatePresence mode="wait">
         {loading && <Loader size="large" title={t("loading")} key="loader" />}
 
         {!loading && (
-          <ScrollArea className="w-full max-w-xl pr-3">
-            <div className="flex flex-col gap-4 pb-4">
+          <div className="scrollable-y w-full max-w-xl overflow-y-scroll pr-0.5">
+            <div className="flex w-full flex-col gap-4 pb-4">
               {parties.map((party, index) => (
                 <PartyCard
                   key={`party-${party.id}`}
@@ -63,21 +62,11 @@ const PartySelection = ({
                 />
               ))}
             </div>
-          </ScrollArea>
+          </div>
         )}
       </AnimatePresence>
     </div>
   );
 };
-
-type PartySelectionCreateDrawerProps = {
-  children: React.ReactNode;
-};
-
-const CreateDrawer = ({ children }: PartySelectionCreateDrawerProps) => {
-  return <>{children}</>;
-};
-CreateDrawer.displayName = "PartySelectionCreateDrawer";
-PartySelection.CreateDrawer = CreateDrawer;
 
 export default PartySelection;
