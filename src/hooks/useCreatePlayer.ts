5697096aeb7e8d33efca1e862dbf0d3d9ec581eb
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 12:10:26 +0100

    :recycle: refactor: useCreatePlayer

--- src/hooks/useCreatePlayer.ts
+++ src/hooks/useCreatePlayer.ts
@@ -1,24 +1,13 @@
 import { createPlayerSchema } from "@/schemas/createPlayer";
-import { DBImmunity } from "@/types/immunitiy";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import overviewTemplate from "@/translations/templates/overview";
 import detailsTemplate from "@/translations/templates/details";
 import i18n from "@/i18next";
-import { DBResistance } from "@/types/resistances";
 
 export const useCreatePlayer = () => {
   const language = i18n.language as "en" | "de";
-  const [picturePreview, setPicturePreview] = useState<string>("");
-  const [refreshKey, setRefreshKey] = useState<number>(0); // to reset the input type file path after a reset
-  const [selectedImmunities, setSelectedImmunities] = useState<DBImmunity[]>(
-    [],
-  );
-  const [selectedResistances, setSelectedResistances] = useState<
-    DBResistance[]
-  >([]);
 
   const formSchema = createPlayerSchema;
   const form = useForm<z.infer<typeof formSchema>>({
@@ -26,7 +15,6 @@ export const useCreatePlayer = () => {
     defaultValues: {
       details: detailsTemplate[language],
       ep: 0,
-      health: 10,
       level: 1,
       maxHealth: 10,
       name: "",
@@ -39,57 +27,5 @@ export const useCreatePlayer = () => {
     },
   });
 
-  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
-    if (!event.target.files) return;
-    const file = event.target.files[0];
-
-    if (file) {
-      setPicturePreview(URL.createObjectURL(file));
-      form.setValue("picture", file);
-    }
-  }
-
-  function handleResetPicture() {
-    form.setValue("picture", "");
-    setRefreshKey((c) => c + 1);
-    setPicturePreview("");
-  }
-
-  function handleAddImmunity(immunity: DBImmunity) {
-    setSelectedImmunities((c) => [...c, immunity]);
-
-    console.log("Added immunity ", immunity);
-    const currentImmunities = form.getValues("immunities");
-    form.setValue("immunities", [...currentImmunities, immunity.id]);
-  }
-
-  function handleAddResistance(resistance: DBResistance) {
-    setSelectedResistances((c) => [...c, resistance]);
-    const currentResistances = form.getValues("resistances");
-    form.setValue("resistances", [...currentResistances, resistance.id]);
-  }
-
-  function handleRemoveImmunity(id: DBImmunity["id"]) {
-    setSelectedImmunities((c) => c.filter((immunity) => immunity.id !== id));
-  }
-
-  function handleRemoveResistance(id: DBResistance["id"]) {
-    setSelectedResistances((c) =>
-      c.filter((resistance) => resistance.id !== id),
-    );
-  }
-
-  return {
-    form,
-    picturePreview,
-    refreshKey,
-    selectedImmunities,
-    selectedResistances,
-    handleRemoveResistance,
-    handleAddResistance,
-    handleFileChange,
-    handleResetPicture,
-    handleAddImmunity,
-    handleRemoveImmunity,
-  };
+  return form;
 };
