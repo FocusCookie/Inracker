cee10f39bfde8b7cdc2ca680bb13085ffaa03e98
FocusCookie <s.lueddemann@mailbox.org>
2025-02-25 11:01:35 +0100

    :recycle: refactor: CreatPlayerDrawer

--- src/components/CreatePlayerDrawer/CreatePlayerDrawer.tsx
+++ src/components/CreatePlayerDrawer/CreatePlayerDrawer.tsx
@@ -1,366 +1,167 @@
-import { useCreatePlayer } from "@/hooks/useCreatePlayer";
-import { ImageFolder } from "@/lib/utils";
-import { createPlayerSchema } from "@/schemas/createPlayer";
-import { TrashIcon } from "@radix-ui/react-icons";
-import { useTranslation } from "react-i18next";
-import { z } from "zod";
+import { storeImage } from "@/lib/utils";
+import { TCreatePlayer } from "@/types/player";
+import CreatePlayerForm from "../CreatePlayerForm/CreatePlayerForm";
 import Drawer from "../Drawer/Drawer";
-import IconPicker from "../IconPicker/IconPicker";
-import ImmunityCard from "../ImmunityCard/ImmunityCard";
-import ResistanceCard from "../ResistanceCard/ResistanceCard";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../ui/form";
-import { Input } from "../ui/input";
-import { ScrollArea } from "../ui/scroll-area";
 import { TypographyH2 } from "../ui/typographyh2";
 import { DBImmunity } from "@/types/immunitiy";
+import ImmunityCard from "../ImmunityCard/ImmunityCard";
 import { DBResistance } from "@/types/resistances";
-import { TCreatePlayer } from "@/types/player";
-import { Textarea } from "../ui/textarea";
-
-type ImmunitiesRenderProps = {
-  selectedImmunities: DBImmunity[];
-  isCreating: boolean;
-  onAddImmunity: (immunity: DBImmunity) => void;
-  onRemoveImmunity: (id: number) => void;
-};
-
-type ResistanceRenderProps = {
-  selectedResistances: DBResistance[];
-  isCreating: boolean;
-  onAddResistance: (resistance: DBResistance) => void;
-  onRemoveResistance: (id: number) => void;
-};
+import ResistanceCard from "../ResistanceCard/ResistanceCard";
+import { useTranslation } from "react-i18next";
 
 type Props = {
-  isCreating: boolean;
   open: boolean;
-  isStoringImage: boolean;
-  renderImmunitiesCatalog: (props: ImmunitiesRenderProps) => React.ReactNode;
-  renderResistancesCatalog: (props: ResistanceRenderProps) => React.ReactNode;
-  onCreate: (player: TCreatePlayer) => void;
+  loading: boolean;
+  /**
+   * the form which controls all the inputs
+   */
+  form: any;
+  immunities: DBImmunity[];
+  resistances: DBResistance[];
   onOpenChange: (state: boolean) => void;
-  onStoringImage: (
-    picture: File | string,
-    folder: ImageFolder,
-  ) => Promise<string | null>;
+  onCreate: (player: TCreatePlayer) => void;
+  onOpenImmunityCatalog: () => void;
+  onOpenResistanceCatalog: () => void;
+  onCreateImmunity: () => void;
+  onCreateResistance: () => void;
 };
 
 function CreatePlayerDrawer({
   open,
-  isCreating,
-  renderImmunitiesCatalog,
-  renderResistancesCatalog,
-  isStoringImage,
-  onCreate,
-  onStoringImage,
+  loading,
+  form,
+  immunities,
+  resistances,
   onOpenChange,
+  onCreate,
+  onOpenImmunityCatalog,
+  onOpenResistanceCatalog,
+  onCreateImmunity,
+  onCreateResistance,
 }: Props) {
   const { t } = useTranslation("ComponentCreatePlayerDrawer");
-
-  const {
-    form,
-    picturePreview,
-    refreshKey,
-    selectedImmunities,
-    selectedResistances,
-    handleRemoveResistance: onRemoveResistance,
-    handleAddResistance: onAddResistance,
-    handleFileChange,
-    handleResetPicture,
-    handleAddImmunity: onAddImmunity,
-    handleRemoveImmunity: onRemoveImmunity,
-  } = useCreatePlayer();
-
-  function handleIconSelect(icon: string) {
-    form.setValue("icon", icon);
-  }
-
-  async function onSubmit(values: z.infer<typeof createPlayerSchema>) {
-    const { picture, health } = values;
-    let pictureFilePath: null | string = null;
+  async function handleCreatePlayer() {
+    const {
+      picture,
+      details,
+      ep,
+      icon,
+      immunities,
+      level,
+      maxHealth,
+      name,
+      overview,
+      resistances,
+      role,
+    } = form.getValues();
+    let pictureFilePath: string | null = null;
 
     if (!!picture) {
-      pictureFilePath = await onStoringImage(picture, "players");
-      console.log({ pictureFilePath });
+      pictureFilePath = await storeImage(picture, "players");
     }
 
     onCreate({
-      ...values,
-      max_health: health,
+      details,
+      ep,
+      icon,
+      immunities,
+      level,
+      name,
+      overview,
+      resistances,
+      role,
+      health: maxHealth,
+      max_health: maxHealth,
       effects: [],
-      image: pictureFilePath,
+      image: pictureFilePath || "",
     });
   }
 
   return (
     <Drawer
-      description={t("descriptionText")}
       open={open}
       onOpenChange={onOpenChange}
+      description={"titleDescription"}
       title={t("title")}
       cancelTrigger={
-        <Button disabled={isCreating || isStoringImage} variant="ghost">
+        <Button disabled={loading} variant="ghost">
           {t("cancel")}
         </Button>
       }
       actions={
-        <Button
-          loading={isCreating || isStoringImage}
-          onClick={form.handleSubmit(onSubmit)}
-        >
+        <Button loading={loading} onClick={handleCreatePlayer}>
           {t("create")}
         </Button>
       }
     >
-      <ScrollArea>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-6"
-          >
-            <div className="flex flex-col gap-4">
-              <TypographyH2>{t("overview")}</TypographyH2>
-
-              <div className="flex items-start gap-2">
-                <div className="flex flex-col gap-1 pt-1.5 pl-0.5">
-                  <FormLabel>{t("icon")}</FormLabel>
-                  <IconPicker
-                    initialIcon={form.getValues("icon")}
-                    disabled={isCreating || isStoringImage}
-                    onIconClick={handleIconSelect}
-                  />
-                  <FormMessage />
-                </div>
-
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }: { field: any }) => (
-                    <FormItem className="w-full px-0.5">
-                      <FormLabel>{t("name")}</FormLabel>
-                      <FormControl>
-                        <Input
-                          disabled={isCreating || isStoringImage}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="role"
-                  render={({ field }: { field: any }) => (
-                    <FormItem className="w-full px-0.5">
-                      <FormLabel>{t("role")}</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="text"
-                          disabled={isCreating || isStoringImage}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="flex items-start gap-2 pl-1">
-                <FormField
-                  control={form.control}
-                  name="level"
-                  render={({ field }: { field: any }) => (
-                    <FormItem className="w-full px-0.5">
-                      <FormLabel>{t("level")}</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          disabled={isCreating || isStoringImage}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="maxHealth"
-                  render={({ field }: { field: any }) => (
-                    <FormItem className="w-full px-0.5">
-                      <FormLabel>{t("health")}</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          disabled={isCreating || isStoringImage}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="flex items-start gap-2 pl-1">
-                <Avatar className="mt-5">
-                  <AvatarImage
-                    src={picturePreview}
-                    alt={t("profilePictureAlt")}
-                  />
-                  <AvatarFallback>
-                    {form.watch("name").slice(0, 2)}
-                  </AvatarFallback>
-                </Avatar>
-
-                <FormItem className="mb-1.5 w-full px-0.5">
-                  <FormLabel>{t("playerPicture")}</FormLabel>
-                  <FormControl>
-                    <div className="flex w-full gap-2">
-                      <Input
-                        key={`refresh-key-${refreshKey}`}
-                        className="grow"
-                        onChange={handleFileChange}
-                        type="file"
-                        disabled={isCreating || isStoringImage}
-                        placeholder={t("picturePlaceholder")}
-                        accept="image/*"
-                      />
-                      {!!picturePreview && (
-                        <Button
-                          type="button"
-                          onClick={handleResetPicture}
-                          variant="destructive"
-                          size="icon"
-                        >
-                          <TrashIcon />
-                        </Button>
-                      )}
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              </div>
-
-              <FormField
-                control={form.control}
-                name="overview"
-                render={({ field }: { field: any }) => (
-                  <FormItem className="flex flex-col gap-1 px-0.5">
-                    <FormLabel>{t("overview")}</FormLabel>
-                    <FormDescription>
-                      {t("overviewDescription")}
-                    </FormDescription>
-
-                    <FormControl className="rounded-md border">
-                      <Textarea
-                        readOnly={isCreating}
-                        {...field}
-                        placeholder="Enter some Overview"
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="details"
-                render={({ field }: { field: any }) => (
-                  <FormItem className="flex flex-col gap-1 px-0.5">
-                    <FormLabel>{t("details")}</FormLabel>
-                    <FormDescription>{t("detailsDescription")}</FormDescription>
-
-                    <FormControl className="rounded-md border">
-                      <Textarea
-                        readOnly={isCreating}
-                        {...field}
-                        placeholder="Enter some description"
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="flex flex-col gap-4">
-              <div className="flex justify-between gap-2">
+      <div className="scrollable-y overflow-y-scroll pr-0.5">
+        <CreatePlayerForm form={form} disabled={loading}>
+          <CreatePlayerForm.Immunities>
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between gap-4">
                 <TypographyH2>{t("immunities")}</TypographyH2>
-                <div className="flex gap-2">
-                  {renderImmunitiesCatalog({
-                    selectedImmunities,
-                    isCreating,
-                    onAddImmunity,
-                    onRemoveImmunity,
-                  })}
+                <div className="flex grow justify-end gap-2">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={onCreateImmunity}
+                  >
+                    {t("create")}
+                  </Button>
+
+                  <Button type="button" onClick={onOpenImmunityCatalog}>
+                    {t("catalog")}
+                  </Button>
                 </div>
               </div>
-              {selectedImmunities.map((immunity) => (
-                <ImmunityCard
-                  key={immunity.id}
-                  immunity={immunity}
-                  actions={
-                    <Button
-                      size="icon"
-                      variant="outline"
-                      onClick={() => onRemoveImmunity(immunity.id)}
-                    >
-                      <TrashIcon />
-                    </Button>
-                  }
-                />
-              ))}
+              <div className="flex flex-col gap-4">
+                {form
+                  .watch("immunities")
+                  .map((id: number) => immunities.find((im) => im.id === id))
+                  .map((immunity: DBImmunity) => (
+                    <ImmunityCard
+                      key={`immunity-${immunity.id}`}
+                      immunity={immunity}
+                    />
+                  ))}
+              </div>
             </div>
+          </CreatePlayerForm.Immunities>
 
-            <div className="flex flex-col gap-4">
-              <div className="flex justify-between gap-2">
-                <TypographyH2>{t("resistances")}</TypographyH2>
-                <div className="flex gap-2">
-                  {renderResistancesCatalog({
-                    selectedResistances,
-                    isCreating,
-                    onAddResistance,
-                    onRemoveResistance,
-                  })}
+          <CreatePlayerForm.Resistances>
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between gap-4">
+                <TypographyH2> {t("resistances")}</TypographyH2>
+                <div className="flex grow justify-end gap-2">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={onCreateResistance}
+                  >
+                    {t("create")}
+                  </Button>
+
+                  <Button type="button" onClick={onOpenResistanceCatalog}>
+                    {t("catalog")}
+                  </Button>
                 </div>
               </div>
-
-              {selectedResistances.map((resistance) => (
-                <ResistanceCard
-                  key={resistance.id}
-                  resistance={resistance}
-                  actions={
-                    <Button
-                      size="icon"
-                      onClick={() => onRemoveResistance(resistance.id)}
-                    >
-                      <TrashIcon />
-                    </Button>
-                  }
-                />
-              ))}
+              <div className="flex flex-col gap-4">
+                {form
+                  .watch("resistances")
+                  .map((id: number) => resistances.find((re) => re.id === id))
+                  .map((resistance: DBResistance) => (
+                    <ResistanceCard
+                      key={`resistance-${resistance.id}`}
+                      resistance={resistance}
+                    />
+                  ))}
+              </div>
             </div>
-          </form>
-        </Form>
-      </ScrollArea>
+          </CreatePlayerForm.Resistances>
+        </CreatePlayerForm>
+      </div>
     </Drawer>
   );
 }
---
+++ src/components/CreatePlayerForm/CreatePlayerForm.tsx
@@ -0,0 +1,245 @@
+import { TrashIcon } from "@radix-ui/react-icons";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import IconPicker from "../IconPicker/IconPicker";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { TypographyH2 } from "../ui/typographyh2";
+
+type Props = {
+  disabled: boolean;
+  form: any;
+  children: React.ReactNode;
+};
+
+type CreatePlayerDrawerCompound = React.FC<Props> & {
+  Immunities: React.FC<{ children: React.ReactNode }>;
+  Resistances: React.FC<{ children: React.ReactNode }>;
+};
+
+const CreatePlayerForm: CreatePlayerDrawerCompound = ({
+  disabled,
+  form,
+  children,
+}) => {
+  const { t } = useTranslation("ComponentCreatePlayerDrawer");
+  const [picturePreview, setPicturePreview] = useState<string>("");
+  const [refreshKey, setRefreshKey] = useState<number>(0); // to reset the input type file path after a reset
+  const childrenArray = React.Children.toArray(children);
+
+  const immunitiesChild = childrenArray.find(
+    (child) =>
+      React.isValidElement(child) && child.type === CreatePlayerForm.Immunities,
+  );
+
+  const resistancesChild = childrenArray.find(
+    (child) =>
+      React.isValidElement(child) &&
+      child.type === CreatePlayerForm.Resistances,
+  );
+
+  function handleResetPicture() {
+    form.setValue("picture", "");
+    setRefreshKey((c) => c + 1);
+    setPicturePreview("");
+  }
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) return;
+    const file = event.target.files[0];
+
+    if (file) {
+      setPicturePreview(URL.createObjectURL(file));
+      form.setValue("picture", file);
+    }
+  }
+
+  function handleIconSelect(icon: string) {
+    form.setValue("icon", icon);
+  }
+
+  // TODO user form.set for setting immunities and resistances
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-6">
+        <div className="flex flex-col gap-4">
+          <TypographyH2>{t("overview")}</TypographyH2>
+
+          <div className="flex items-start gap-2">
+            <div className="flex flex-col gap-1 pt-1.5 pl-0.5">
+              <FormLabel>{t("icon")}</FormLabel>
+              <IconPicker
+                initialIcon={form.getValues("icon")}
+                disabled={disabled}
+                onIconClick={handleIconSelect}
+              />
+              <FormMessage />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }: { field: any }) => (
+                <FormItem className="w-full px-0.5">
+                  <FormLabel>{t("name")}</FormLabel>
+                  <FormControl>
+                    <Input disabled={disabled} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field }: { field: any }) => (
+                <FormItem className="w-full px-0.5">
+                  <FormLabel>{t("role")}</FormLabel>
+                  <FormControl>
+                    <Input type="text" disabled={disabled} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex items-start gap-2 pl-1">
+            <FormField
+              control={form.control}
+              name="level"
+              render={({ field }: { field: any }) => (
+                <FormItem className="w-full px-0.5">
+                  <FormLabel>{t("level")}</FormLabel>
+                  <FormControl>
+                    <Input type="number" disabled={disabled} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="maxHealth"
+              render={({ field }: { field: any }) => (
+                <FormItem className="w-full px-0.5">
+                  <FormLabel>{t("health")}</FormLabel>
+                  <FormControl>
+                    <Input type="number" disabled={disabled} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex items-start gap-2 pl-1">
+            <Avatar className="mt-5">
+              <AvatarImage src={picturePreview} alt={t("profilePictureAlt")} />
+              <AvatarFallback>{form.watch("name").slice(0, 2)}</AvatarFallback>
+            </Avatar>
+
+            <FormItem className="mb-1.5 w-full px-0.5">
+              <FormLabel>{t("playerPicture")}</FormLabel>
+              <FormControl>
+                <div className="flex w-full gap-2">
+                  <Input
+                    key={`refresh-key-${refreshKey}`}
+                    className="grow"
+                    onChange={handleFileChange}
+                    type="file"
+                    disabled={disabled}
+                    placeholder={t("picturePlaceholder")}
+                    accept="image/*"
+                  />
+                  {!!picturePreview && (
+                    <Button
+                      type="button"
+                      onClick={handleResetPicture}
+                      variant="destructive"
+                      size="icon"
+                    >
+                      <TrashIcon />
+                    </Button>
+                  )}
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </div>
+
+          <FormField
+            control={form.control}
+            name="overview"
+            render={({ field }: { field: any }) => (
+              <FormItem className="flex flex-col gap-1 px-0.5">
+                <FormLabel>{t("overview")}</FormLabel>
+                <FormDescription>{t("overviewDescription")}</FormDescription>
+
+                <FormControl className="rounded-md border">
+                  <Textarea
+                    readOnly={disabled}
+                    {...field}
+                    placeholder="Enter some Overview"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="details"
+            render={({ field }: { field: any }) => (
+              <FormItem className="flex flex-col gap-1 px-0.5">
+                <FormLabel>{t("details")}</FormLabel>
+                <FormDescription>{t("detailsDescription")}</FormDescription>
+
+                <FormControl className="rounded-md border">
+                  <Textarea
+                    readOnly={disabled}
+                    {...field}
+                    placeholder="Enter some description"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        {immunitiesChild}
+
+        {resistancesChild}
+      </form>
+    </Form>
+  );
+};
+
+CreatePlayerForm.Immunities = ({ children }) => {
+  return <>{children}</>;
+};
+CreatePlayerForm.Immunities.displayName = "ChapterLayout.Immunities";
+
+CreatePlayerForm.Resistances = ({ children }) => {
+  return <>{children}</>;
+};
+CreatePlayerForm.Resistances.displayName = "ChapterLayout.Resistances";
+
+export default CreatePlayerForm;
