568e5e28d79cdd3098531b5b9b07d5f90614a85b
FocusCookie <s.lueddemann@mailbox.org>
2025-06-03 14:10:42 +0200

    :sparkle: feat: ResistanceCatalog create button

--- src/components/ResistancesCatalog/ResistancesCatalog.tsx
+++ src/components/ResistancesCatalog/ResistancesCatalog.tsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import Catalog from "../Catalog/Catalog";
 import ResistanceCard from "../ResistanceCard/ResistanceCard";
 import { Button } from "../ui/button";
+import { useTranslation } from "react-i18next";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { MoonIcon } from "@radix-ui/react-icons";
+import { useResistancesStore } from "@/stores/useResistanceStore";
+import { useShallow } from "zustand/shallow";
 
 type Props = {
   resistances: DBResistance[];
@@ -13,16 +18,26 @@ type Props = {
 
 function ResistancesCatalog({ open, resistances, onAdd, onOpenChange }: Props) {
   const [search, setSearch] = useState<string>("");
+  const { t } = useTranslation("ComponentResistanceCatalog");
 
-  //TODO: if resistances are empty show create button
+  const { openCreateResistanceDrawer } = useResistancesStore(
+    useShallow((state) => ({
+      openCreateResistanceDrawer: state.openCreateResistanceDrawer,
+    })),
+  );
+
+  function handleCreateResistance() {
+    onOpenChange(false);
+    openCreateResistanceDrawer();
+  }
 
   return (
     <Catalog
       open={open}
       onOpenChange={onOpenChange}
-      title={"Resistances"}
-      description={"choose an resistance"}
-      placeholder={"search for a specific resistance"}
+      title={t("resistances")}
+      description={t("description")}
+      placeholder={t("placeholderSearch")}
       search={search}
       onSearchChange={setSearch}
     >
@@ -39,6 +54,21 @@ function ResistancesCatalog({ open, resistances, onAdd, onOpenChange }: Props) {
             actions={<Button onClick={() => onAdd(resistance)}>add</Button>}
           />
         ))}
+
+      {resistances.length === 0 && (
+        <Alert>
+          <MoonIcon />
+          <AlertTitle>{t("noResistances")}</AlertTitle>
+          <AlertDescription>
+            {t("noResistancesDescription")}
+            <div className="flex w-full justify-center">
+              <Button onClick={handleCreateResistance} className="mt-2">
+                {t("createResistance")}
+              </Button>
+            </div>
+          </AlertDescription>
+        </Alert>
+      )}
     </Catalog>
   );
 }
