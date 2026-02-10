18f447081b92c22cb8712d84ff7874480b1a6206
FocusCookie <s.lueddemann@mailbox.org>
2026-02-10 10:14:08 +0100

    :bug: fix: windows icon issue

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
+use tauri_build::{Attributes, WindowsAttributes};
+
 fn main() {
-    tauri_build::build()
+    tauri_build::try_build(
+        Attributes::new()
+            .windows_attributes(WindowsAttributes::new().window_icon_path("icons/icon.ico")),
+    )
+    .expect("failed to run tauri-build");
 }
